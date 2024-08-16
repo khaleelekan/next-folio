362237be3862d8269246eb7efe534276040dfab0
khalilullahi abdulraheem <kalilulekan@gmail.com>
2024-08-16 12:11:46 +0100

    new

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,10 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -70,4 +74,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
